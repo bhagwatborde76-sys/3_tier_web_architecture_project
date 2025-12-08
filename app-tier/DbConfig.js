a6675f03a1e17eff50ae4c5f9457965829262e24
bhagwatborde76-sys <bhagwatborde76@gmail.com>
2025-12-08 17:16:18 +0530

    Add files via upload

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-2.cp0ci0aoac2r.ap-northeast-1.rds.amazonaws.com',
+    DB_USER : 'new',
+    DB_PWD : 'linux0095',
+    DB_DATABASE : 'webappdb'
+});
